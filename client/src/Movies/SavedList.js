1214d8e8428abf3001d1c52ae6d67ba487f94800
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-11-13 19:02:48 -0500

    MVP 4 real yo

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -7,8 +7,8 @@ const SavedList = props => (
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-      <Link to= "/"> 
-    <div className="home-button">Home</div> 
+    <Link to= "/"> 
+      <div className="home-button">Home</div> 
     </Link>
   </div>
 );
